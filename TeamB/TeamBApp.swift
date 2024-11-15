b8385157d309633c85821020f198e6970a6c1aa3
yukiny0811 <yukiny0811@gmail.com>
2024-11-15 22:07:36 +0900

    Initial Commit

---
+++ TeamB/TeamBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamBApp.swift
+//  TeamB
+//
+//  Created by Yuki Kuwashima on 2024/11/15.
+//
+
+import SwiftUI
+
+@main
+struct TeamBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
